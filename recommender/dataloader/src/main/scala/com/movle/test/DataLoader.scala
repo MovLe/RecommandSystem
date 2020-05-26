bfd69b1b1638c493acc12f2853bf815bdf3b1438
Movle <richard.xiu.520@gmail.com>
2020-05-26 10:25:23 +0800

    新建DataLoader

---
+++ recommender/dataloader/src/main/scala/com/movle/test/DataLoader.scala
@@ -0,0 +1,16 @@
+package com.movle.test
+
+/**
+ * @ClassName DataLoader
+ * @MethodDesc: TODO DataLoader功能介绍
+ * @Author Movle
+ * @Date 5/26/20 10:24 上午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+object DataLoader {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
